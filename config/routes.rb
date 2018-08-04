d3ed1f5d4bd87436932d88a7f16171d4512f02c2
SPDUK <spdevuk@gmail.com>
2018-08-05 00:51:11 +0100

    changed routes & added gems for heroku deploy

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # pages/home route displays the home .erb file inside the views/pages folder
-  get '/pages/home',  to: 'pages#home'
-  get '/pages/about', to: 'pages#about'
+  root 'pages#home'
+  get '/about', to: 'pages#about'
 end
