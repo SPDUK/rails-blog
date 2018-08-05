0c8e9f69840fca9734ff76094dcd0f7cee654058
SPDUK <spdevuk@gmail.com>
2018-08-05 14:00:00 +0100

    added in error messages for the form

--- app/models/articles_controller.rb
+++ app/models/articles_controller.rb
@@ -4,6 +4,19 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = 'Article was saved'
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def article_params
+    # top level key article
+    params.require(:article).permit(:title, :description)
   end
 end
