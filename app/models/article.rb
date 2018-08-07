bc2b3d10c66bf7476e6b89f11cc7d3932ed7e987
SPDUK <spdevuk@gmail.com>
2018-08-07 06:16:36 +0100

    oops, fixed errors and added comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@
 
 class Article < ActiveRecord::Base
   # articles have a single user
-  belogs_to :user
+  belongs_to :user
   validates :title, presence: true, length: { minimum: 3, maximum: 50 }
   validates :description, presence: true, length: { minimum: 10, maximum: 300 }
   validates :user_id, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,12 @@ class User < ActiveRecord::Base
   # User has many articles
   has_many :articles
 
+  # User.email is lowercased before saving
   before_save { self.email = email.downcase }
   # User.username - required, unique case insensitive, length 3-20
   validates :username, presence: true,
                        uniqueness: { case_sensitive: false },
                        length: { minimum: 3, maximum: 20 }
-
   # User.email - required, unique case insensitive, length max 164, must be email
   validates :email, presence: true, length: { maximum: 164 },
                     uniqueness: { case_sensitive: false },
