6e1a998be1017f9dfc74b9354b1ba8a55da0a578
SPDUK <spdevuk@gmail.com>
2018-08-07 05:40:40 +0100

    added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
+  # User.username - required, unique case insensitive, length 3-20
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: {minimum: 3, maximum: 20}
 
+  # User.email - required, unique case insensitive, length max 164, must be email
+  validates :email, presence: true, length: {maximum: 164},
+   uniqueness: { case_sensitive: false},
+   format: {with: URI::MailTo::EMAIL_REGEXP }
 end
