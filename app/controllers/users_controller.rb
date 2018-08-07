c20a823f159ce1435d68a328a455580e828f3df8
SPDUK <spdevuk@gmail.com>
2018-08-07 13:33:58 +0100

    Added user signup form routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,24 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:success] = "Welcome to blogs on rails #{@user.username}"
+      redirect_to articles_path
+    else
+      # figure out why this route changes to /users
+      render 'new'
+    end
+
+  end
+
+  private
+
+  def user_params
+    # whitelist
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
