2c523c5024671612c29e59629941c5ef365820f7
SPDUK <spdevuk@gmail.com>
2018-08-08 14:02:05 +0100

    conditional navbar rendering of login/signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,25 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    # return current user if current user already exists and have looked for it previously
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    # true if a current user exists else false
+    !!current_user
+  end
+
+  def require_user
+    # restricted actions
+    if !logged_in?
+      flash[:danger] = "You must be logged in to perform that action"
+      redirect_to root_path
+    end
+  end
 end
