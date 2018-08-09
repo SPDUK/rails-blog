d7c98bea8ffc6830dbb3f8abae735c87e5ad57ab
SPDUK <spdevuk@gmail.com>
2018-08-09 17:09:10 +0100

    actually fixed this time

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -65,7 +65,7 @@ class UsersController < ApplicationController
   end
 
   def require_same_user
-    unless logged_in? && current_user != @user || !current_user.admin?
+    if (current_user != @user) && !current_user.admin?
       flash[:danger] = 'You can only edit your own account'
       redirect_to root_path
     end
@@ -73,7 +73,7 @@ class UsersController < ApplicationController
 
   def require_admin
     if logged_in? && !current_user.admin?
-      flash[:danger] = "Only admin users can perform that action"
+      flash[:danger] = 'Only admin users can perform that action'
       redirect_to root_path
     end
   end
