21ce0964ee2246f0bf03613228cfb732ebfe38f3
SPDUK <spdevuk@gmail.com>
2018-08-10 19:37:28 +0100

    even more categories tests

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -24,10 +24,9 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
     assert_template "categories/new"
     assert_no_difference "Category.count" do
       post categories_path, params: {category: {name: ""}}
-      follow_redirect!
     end
     assert_template "categories/new"
-    assert_select 'h2.panel-title'
-    assert_select 'div.panel-body'
+    assert_select 'div.alert-heading'
+    assert_select 'h2'
   end
 end
